c12c5e5912bbbf0ff6983d6c8dc3365b124ed1b7
Alex Ryan <alex@alexryan.org>
2016-04-15 01:02:38 -0700

    Parse and print port from config file, but not actually using it yet

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -23,6 +23,20 @@ string content("HTTP/1.1 200 OK\r\n"
                "\r\n"
                "<html><body>Hello, world!</body></html>");
 
+/* Citation: this function courtesy of the provided sample solution */
+int getPort(const NginxConfig &config) {
+  for (const auto& statement : config.statements_) {
+    bool kl = true;
+    for (const std::string& token : statement->tokens_) {
+      if (!kl) {
+        try { return stoi(token); } catch (...) {}
+      }
+      kl = (token != "port");
+    }
+  }
+  return -1;
+}
+
 void accept_handler(const boost::system::error_code &ec);
 void write_handler(const boost::system::error_code &ec, std::size_t num_bytes);
 
@@ -43,10 +57,26 @@ void accept_handler(const boost::system::error_code &ec) {
     async_write(tcp_socket, buffer(content), write_handler);
 }
 
-int main() {
+int main(int argc, char **argv) {
+    /* Citation: these 14 lines courtesy of the provided sample solution */
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " config_file" << endl;
+        return -1;
+    }
+    NginxConfigParser config_parser;
+    NginxConfig config;
+    if (!config_parser.Parse(argv[1], &config)) {
+        return -1;
+    }
+    int port = getPort(config);
+    if (port < 1) {
+        cerr << "Couldn't find a port in the config file." << endl;
+        return -1;
+    }
+
     tcp_acceptor.listen();
     tcp_acceptor.async_accept(tcp_socket, accept_handler);
-    cout << "Server started, listening on port " << "8000" << endl;
+    cout << "Server started, listening on port " << port << endl;
     ioservice.run();
     return 0;
 }
